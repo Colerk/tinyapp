3585b205f681042ab608eee56ac70d065323702a
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-17 19:25:08 +0000

    added register page with form

--- express_server.js
+++ express_server.js
@@ -11,6 +11,13 @@ let cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_register", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -31,6 +38,8 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
